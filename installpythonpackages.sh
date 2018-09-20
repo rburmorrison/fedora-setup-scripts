47327399182508dcdcd8b6c7681b9119feedfabb
Ryan Burmeister-Morrison <12453590+rburmorrison@users.noreply.github.com>
2018-09-20 13:14:55 -0400

    Update installpythonpackages.sh

--- installpythonpackages.sh
+++ installpythonpackages.sh
@@ -4,5 +4,5 @@ pip install --upgrade pip
 # Install common packages
 pip install msgpack requests flask scrapy pylint \
 	pygame scapy pyqt5 ipython nose twisted \
-	pep8 autopep8 pyqtdeploy pyshark
+	pep8 autopep8 pyqtdeploy pyshark pandoc
 
