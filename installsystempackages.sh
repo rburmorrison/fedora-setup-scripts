ebe71898639f2aee89b5fcd7c20d37ef47b03ced
Ryan Burmeister-Morrison <12453590+rburmorrison@users.noreply.github.com>
2018-09-21 11:50:39 -0400

    Update installsystempackages.sh

--- installsystempackages.sh
+++ installsystempackages.sh
@@ -2,10 +2,14 @@ sudo dnf groupinstall -y "Development Tools"
 
 # Add repos and keys
 sudo rpm --import https://build.opensuse.org/projects/home:manuelschneid3r/public_key
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 sudo dnf config-manager --add-repo https://download.opensuse.org/repositories/home:manuelschneid3r/Fedora_28/home:manuelschneid3r.repo
 sudo dnf config-manager --add-repo https://download.opensuse.org/repositories/home:/phiwo:/peek/Fedora_26/home:phiwo:peek.repo
 sudo dnf config-manager --add-repo=https://copr.fedorainfracloud.org/coprs/dirkdavidis/papirus-icon-theme/repo/fedora-28/dirkdavidis-papirus-icon-theme-fedora-28.repo
+
+sudo dnf check-update
 
 # Install ffmpeg
 sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
@@ -68,7 +72,8 @@ sudo dnf install -y sqlitebrowser \
 	shutter \
 	pandoc \
 	vte libgda-ui \
-	menulibre
+	menulibre \
+	code
 
 # Development Packages
 sudo dnf install -y gtk+-devel \
